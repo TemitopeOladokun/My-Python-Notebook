7d2a87c9ac9d72f68496330c14f5dcb7934d1d46
Temitope Oladokun <50429511+TemitopeOladokun@users.noreply.github.com>
2020-10-05 16:07:27 +0100

    Add files via upload

---
+++ First.py
@@ -0,0 +1,17 @@
+name = input("What is your name ?")
+Color = input("What is your favourite color ?")
+print(name + " likes " + Color)
+
+birth_year = input("Birth year: ")
+age = 2020 - int(birth_year)
+print(age)
+
+
+weight = input("What is your weight in lbs ?")
+Kilogram = (int(weight) * 0.45)
+print(str(Kilogram) + "kg")
+
+
+weight = input("What is your weight in lbs ?")
+Kilogram = (float(weight) * 0.45)
+print(str(Kilogram) + "kg")
