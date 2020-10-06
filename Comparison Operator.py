55de996d60d349e875ef87fe4136433be21e620f
Temitope Oladokun <50429511+TemitopeOladokun@users.noreply.github.com>
2020-10-06 09:25:35 +0100

    Add files via upload

---
+++ Comparison Operator.py
@@ -0,0 +1,99 @@
+'''
+# Greater than
+temperature = 30
+
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+temperature = 35
+
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+# Less than
+temperature = 30
+
+if temperature < 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+temperature = 35
+
+if temperature < 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+#Less than and equal to
+temperature = 35
+
+if temperature <= 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+#Greater than and Equal to
+temperature = 35
+
+if temperature >= 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+#Equality sign
+temperature = 30
+
+if temperature == 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+print("\n")
+
+'''
+
+
+# EXERCISE
+name = 30
+#namey = 
+if name < 3:
+    print("Name is too short")
+elif name > 50:
+    print("It cannot exceed 50 character")
+else:
+    print("Names looks good")
+
+
+name = input("Enter your name ?")
+#print(len(name))
+
+character = len(name)
+print(character)
+
+if character < 3:
+    print("Name is too short")
+elif character > 50:
+    print("Name is too long")
+else:
+    print("Ideal name format")
+
+
+
+
