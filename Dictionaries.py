a908238874e78e9ea3fe47cf7ec41dc95340ddfa
Temitope Oladokun <50429511+TemitopeOladokun@users.noreply.github.com>
2020-10-13 00:26:27 +0100

    First Commit

---
+++ Dictionaries.py
@@ -0,0 +1,10 @@
+customer = {
+    "name" : "John Smith",
+    "age" : 30,
+    "email" : "john@gmail.com",
+    "Nationality" : "Nigerian",
+    "is_verified" : True
+
+}
+print(customer["name"])
+print(customer["email"])
