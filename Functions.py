bd6810f4976cb8c995c5d7fd9c81aad1f34d5901
Temitope Oladokun <50429511+TemitopeOladokun@users.noreply.github.com>
2020-10-13 13:19:50 +0100

    Second Commit

--- Functions.py
+++ Functions.py
@@ -1,3 +1,5 @@
+'''
+# Without parameters
 def greet_user():
     print("Welcome on board")
     print("This is just the beginning")
@@ -5,6 +7,29 @@ def greet_user():
 
 print("Start")
 greet_user()
+print("Finish")
+
+
+# With parameters
+def greet_user2(name):
+    print("Hi %s" % (name))
+    print("Welcome onboard")
+
+
+print("Start")
+greet_user2("Temitope")
+print("Finish")
+
+'''
+
+# With 2 parameters
+def greet_user3(first_name, last_name):
+    print("Hi %s %s" % (first_name, last_name))
+    print("Welcome onboard")
+
+
+print("Start")
+greet_user3("Temi", "Tope")
 print("Finish")
 
 
