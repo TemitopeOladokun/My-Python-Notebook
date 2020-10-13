c4589257a4f1951405040d96f2d49dbf0f69b1f5
Temitope Oladokun <50429511+TemitopeOladokun@users.noreply.github.com>
2020-10-13 13:36:33 +0100

    First Commit

---
+++ Keyword Argument.py
@@ -0,0 +1,10 @@
+# With 2 parameters
+def greet_user3(first_name, last_name):
+    print("Hi %s %s" % (first_name, last_name))
+    print("Welcome onboard")
+
+
+print("Start")
+greet_user3(last_name="Tope", first_name="Temi")   # Keyword Argument
+print("Finish")
+
